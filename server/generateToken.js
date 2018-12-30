aaee2805302017f8d75a0690ce0bb539d05d135f
cal <callummac@protonmail.com>
2018-12-30 14:52:19 +0000

    Extended token expiration time to 1 week

--- server/generateToken.js
+++ server/generateToken.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const jwt_key = "secret";
 
 const generateToken = data => {
-    return jwt.sign(data, jwt_key, { expiresIn: "1hour" });
+    return jwt.sign(data, jwt_key, { expiresIn: "7d" });
 };
 
 module.exports = generateToken;

458a3b1d6d65dc9c76e10e352e95d2e2394c14f3
cal <callummac@protonmail.com>
2019-01-13 18:13:48 +0000

    Exported vue component into seperate files.

--- src/js/vue/form.js
+++ src/js/vue/form.js
@@ -1,4 +1,6 @@
 import Vue from 'vue';
+import checkForm from './methods/checkForm';
+import sendRequest from './methods/sendRequest';
 
 const form = new Vue({
 	el: '#registerForm',
@@ -17,62 +19,8 @@ const form = new Vue({
 		emailTaken: false,
 	},
 	methods: {
-		toggleLabel: function (e) {
-			console.log('toggle e', e);
-		},
-		checkForm: function (e) {
-			e.preventDefault();
-
-			this.submitted = true;
-			this.emailTaken = false;
-			this.errors = {};
-
-			if (!this.email) this.errors.email = 'Email required.';
-			else if (!this.email.match(new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/))) this.errors.email = 'Email Invalid.';
-
-			if (!this.email_conf) this.errors.email_conf = 'Confirm email required.';
-			else if (this.email_conf !== this.email) this.errors.email_conf = 'Email fields don\'t match.';
-
-			if (!this.fname) this.errors.fname = 'First name required.';
-			else if (this.fname.length > 20) this.errors.fname = 'First name max length 20 characters.';
-
-			if (!this.lname) this.errors.lname = 'Last name required.';
-			else if (this.lname.length > 20) this.errors.lname = 'Last name max length 20 characters.';
-
-			if (!this.password) this.errors.password = 'Password required.';
-			else if (this.password.length < 8 || this.password.length > 100) this.errors.password = 'Password must be 8-100 characters.';
-
-			if (!this.password_conf) this.errors.password_conf = 'Confirm password required.';
-			else if (this.password_conf !== this.password) this.errors.password_conf = 'Password fields don\'t match.';
-
-			if (Object.keys(this.errors).length === 0) this.sendRequest(this.email, this.password, this.fname, this.lname);
-		},
-		sendRequest(email, password, fname, lname) {
-			this.loading = true;
-
-			const sendData = { email, fname, lname, password };
-
-			fetch('http://localhost:3000/register', {
-				method: 'post',
-				headers: {
-					'Accept': 'application/json',
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify(sendData)
-			})
-				.then(res => {
-					this.loading = false;
-
-					if (res.status === 404) this.emailTaken = true;
-					if (res.status === 200) {
-						window.location = '/success';
-					}
-				})
-				.catch(err => {
-					this.loading = false;
-					console.log('fetch err', err);
-				});
-		}
+		checkForm,
+		sendRequest
 	}
 });
 
---
+++ src/js/vue/methods/sendRequest.js
@@ -0,0 +1,28 @@
+const sendRequest = function (email, password, fname, lname) {
+	this.loading = true;
+
+	const sendData = { email, fname, lname, password };
+
+	fetch('http://localhost:3000/register', {
+		method: 'post',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(sendData)
+	})
+		.then(res => {
+			this.loading = false;
+
+			if (res.status === 404) this.emailTaken = true;
+			if (res.status === 200) {
+				window.location = '/success';
+			}
+		})
+		.catch(err => {
+			this.loading = false;
+			console.log('fetch err', err);
+		});
+};
+
+export default sendRequest;

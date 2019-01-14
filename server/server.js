417a85693acf13700b97afa746b492d87355db72
cal <callummac@protonmail.com>
2019-01-14 10:15:02 +0000

    Bug fixing for mongo URI error

--- server/server.js
+++ server/server.js
@@ -20,6 +20,9 @@ require('./handleSocket')(io);
 
 const mongoose = require('mongoose');
 const DB_URL = !!process.env.DB_URL ? process.env.DB_URL : 'mongodb://localhost/chat';
+
+console.log('DB_URL', DB_URL, process.env.DB_URL);
+
 mongoose.connect(DB_URL, { useNewUrlParser: true });
 
 const rooms = require('./rooms');
